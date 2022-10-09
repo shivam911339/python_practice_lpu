321e616d66c4063d00b57453d72ef8430e0506d2
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-10-09 22:44:18 +0530

    marks

---
+++ elif.py
@@ -0,0 +1,9 @@
+marks= int(input("Enter your marks:"))
+if marks>90 and marks<=100:
+    print("your grade is A")
+elif marks>80 and marks<=90:
+    print("your grade is B")
+elif marks>70 and marks<=80:
+    print("ypur grade is C")
+else:
+    print("you are fail")        
