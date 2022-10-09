da1a40cf45e125ffeb8fbc35ae005923bc73a7ec
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-10-09 23:35:32 +0530

    greatestnuber

---
+++ greatestNumber.py
@@ -0,0 +1,10 @@
+#Take two inputs from user(int/float)
+#print the greatest number
+number1=int(input("enter first number"))
+number2=int(input("enter second number"))
+if number1>number2:
+    print("first is greatest")
+elif number2>number1:
+    print("second is greatest")
+else:
+    print("not defined") 
