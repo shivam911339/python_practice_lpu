91334ba12b86f8ed27efa58721982cc8d4bb7894
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-10-14 21:49:17 +0530

    variables

---
+++ tonyStark.py
@@ -0,0 +1,11 @@
+from http.client import TOO_MANY_REQUESTS
+
+
+firstname =("Tony")
+secondName = ("Stark")
+age = 51
+Is_Tony_genius = True
+print(firstname)
+print(secondName)
+print(age)
+print(Is_Tony_genius)
