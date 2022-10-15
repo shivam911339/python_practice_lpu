695e9f3d33ab0ae6b780a89138d2848119b7a146
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-10-15 18:18:55 +0530

    check string/charecter

---
+++ in.py
@@ -0,0 +1,9 @@
+#to check if a charecter/string is part of main string
+name= "shivam kumar tiwari"
+print("shivam" in name)
+print("kumar" in name)
+print("tiwari" in name)
+print("s" in name)
+print("w" in name)
+print("akash" in name)
+print("p" in name)
