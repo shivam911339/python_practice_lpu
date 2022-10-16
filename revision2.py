d24a626111746277879465bdf15de7d8a531d6a5
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-10-16 12:23:23 +0530

    revision2

---
+++ revision2.py
@@ -0,0 +1,8 @@
+#take user take three side of triangle
+a=int(input("Enter number1 here:"))
+b=int(input("Enter number2 here:"))
+c=int(input("Enter number3 here:"))
+if a+b==c:
+    print("it is triangle")
+else:
+    print("it is not triangle")    
