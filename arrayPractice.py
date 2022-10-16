0a58442d6cdaf331428fff1c233f84c5a53fd170
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-10-16 13:52:59 +0530

    array1

---
+++ arrayPractice.py
@@ -0,0 +1,11 @@
+a= "A quick brown fox jumps over the lazy dog"
+print(a[6])
+print(a[0:5])
+print(a[5:11])
+print(len(a))
+
+
+if "fox" in a:
+    print("yes is it there")
+else:
+    print("no is it not there")    
